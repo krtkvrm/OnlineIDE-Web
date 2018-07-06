1aae7ad7a3c667e44d590ff58ed5715278fd1817
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-07-06 23:47:31 +0530

    Program from Editor and Language Selection

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,12 +1,29 @@
-require.config({ paths: { 'vs': 'includes/monaco-editor/min/vs' }});
-require(['vs/editor/editor.main'], function() {
-	var editor = monaco.editor.create(document.getElementById('container'), {
-		value: [
-			'function x() {',
-			'\tconsole.log("Hello world!");',
-			'}'
-		].join('\n'),
-		language: 'javascript',
-		theme: 'hc-black'
+var editor = null
+
+const loadEditor = (lang) => {
+	require.config({ paths: { 'vs': 'includes/monaco-editor/min/vs' }})
+	require(['vs/editor/editor.main'], () => {
+		editor = monaco.editor.create(document.getElementById('container'), {
+			value: [
+				'#include<stdio.h>',
+				'int main {',
+				'\treturn 0;',
+				'}'
+			].join('\n'),
+			language: lang,
+			theme: 'hc-black'
+		})
 	})
+}
+
+loadEditor(document.getElementById('lang').value)	
+
+$(window).on('load', () => {
+
+		// Change Lanuage
+		$('select').on('change', () => {
+			$('#container').empty()
+			loadEditor(document.getElementById('lang').value)
+		})
+
 })
